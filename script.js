f179d163fd76a2604fd190c5173ef2083d8b9d07
Tim Lacault <lacault.tim@gmail.com>
2023-05-11 23:37:22 +0200

    [Add]: added favicon, about_me section
    [Update]: modified scss architecture

--- script.js
+++ script.js
@@ -24,11 +24,22 @@ function go_to_section(section) {
 }
 
 
-const circle = document.querySelector('.circle_1');
+const circle = document.querySelector('.home_cir_1');
 
 document.addEventListener('mousemove', (event) => {
     const mouseX = event.clientX;
     const mouseY = event.clientY;
 
     circle.style.transform = `translate(${mouseX}px, ${mouseY}px) translate(-50%, -50%)`;
+});
+
+
+window.addEventListener('scroll', function () {
+    var scrollPosition = window.pageYOffset;
+
+    if (scrollPosition >= 1500) {
+        document.querySelector('.background').style.background = 'rgba(216, 222, 233, 1)';
+    } else {
+        document.querySelector('.background').style.background = 'rgba(67, 76, 94, 1)';
+    }
 });
